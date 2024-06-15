89574c2db8d58f7f22adc008141f37ec8aa4b3ed
Ishak Hadj Mohammed <izakreeducation@gmail.com>
2024-06-15 10:44:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ local_css("style/style.css")
 
 # ---- LOAD ASSETS ----
 lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
-img_contact_form = Image.open("images\\izak.jpg")
-img_lottie_animation = Image.open("images\\izak1.jpg")
+img_contact_form = Image.open("images//izak.jpg")
+img_lottie_animation = Image.open("images//izak1.jpg")
 
 # ---- HEADER SECTION ----
 with st.container():
